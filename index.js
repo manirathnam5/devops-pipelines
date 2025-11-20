95fbdddaf429b7327daa0d8c2f12d052ddfda2f9
manirathnam5 <manirathnam501@gmail.com>
2025-11-20 19:36:02 +0530

    feat:add ci pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello World ");
+console.lof("Learning ci/cd piplines using Github Actions");
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("Starting tests");
+console.log(()=> console.log("Waiting for 3 seconds"),3000);
+console.log("Test complete");
